17a96568ebf24ac649e8429c2a9817c0282ba9cf
Alondra87 <aa5887687@gmail.com>
2022-06-27 16:12:20 -0400

    Adding Books

---
+++ .github/workflows/script.js
@@ -0,0 +1,33 @@
+const initialBooks = [
+    {
+        id: 1,
+        title: "New Book",
+        author: "Author1"
+    },
+    {
+        id: 2,
+        title: "New Project",
+        author: "Author2"
+    }
+]
+
+console.log(initialBooks);
+
+function displayBooks() {
+    const loadBooks = `
+ <ul>
+ <li> 
+ ${initialBooks[i].title}
+ </li>
+ </ul>`
+
+
+}
+
+
+const books = document.getElementById('books')
+
+function addBooks(title, author, id) {
+
+
+}
